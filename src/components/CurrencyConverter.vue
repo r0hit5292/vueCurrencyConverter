7fb1149cb67fe255b21e86219534a195c9132290
Rohit Sachan <rohit.sachan@vidyamantra.com>
2022-11-16 12:34:22 +0530

    currency converter

--- src/components/CurrencyConverter.vue
+++ src/components/CurrencyConverter.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class=" bg-black" >
+    <div>
      
      <input v-model="INPUT"  type="number" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username"  placeholder="enter currency">
  
